bef6a514abf8030a8e796f2f6735935213b79947
leipengfei6 <leipengfei6.hikvision.com.cn>
2019-03-24 01:12:09 +0800

    配置文件更新

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -35,12 +35,12 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
+      '@': path.join(__dirname, '..'),
     }
   },
   module: {
     rules: [
-      ...(config.dev.useEslint ? [createLintingRule()] : []),
+      // ...(config.dev.useEslint ? [createLintingRule()] : []),
       {
         test: /\.vue$/,
         loader: 'vue-loader',
